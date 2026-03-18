d7d3b35360c0d1522a315aff72b73985f3505af8
Whoscher <whoscher@MacBook-Air-Whoscher.local>
2026-03-18 20:47:15 +0300

    docs:add project documentation

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-SERVICE="Dock"
+
+# Если мы передали имя сервиса при запуске - этот сервис будет использоваться, если нет- ,будет bash
+
+SERVICE=${1:-"bash"}
+
 if pgrep -x "$SERVICE" > /dev/null
 then 
 	echo "$(date) - $SERVICE is UP" >> monitor.log
 else
-	echo echo "$(date) - $SERVICE is DOWN" >> monitor.log
+	echo "$(date) - $SERVICE is DOWN" >> monitor.log
 fi
 cat monitor.log
